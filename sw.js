7f9b37125bc4352ca8b848d4f783d2476a013e55
ybensellam <80487919+ybensellam@users.noreply.github.com>
2026-07-09 09:33:00 +0200

    Update cache version from v4 to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="patrimoine-v4";
+const CACHE="patrimoine-v5";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
